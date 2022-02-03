9bdb744c20e2e75257b6090d8447c12859596b5e
shinigami723 <98699572+shinigami723@users.noreply.github.com>
2022-02-03 14:12:36 +0530

    Create COVID-19_data_plot.m
    Creates a line chart, area plot and bar graph for the COVID-19 data set from ourworldindata.com

---
+++ code_work/COVID-19_data_plot.m
@@ -0,0 +1,58 @@
+%Initialisation
+clc
+clear
+close all
+
+%Fetching the data
+data = webread('https://covid.ourworldindata.org/data/owid-covid-data.csv');
+
+%%
+data.location = categorical(data.location);
+date_reported = data.date;
+country = data.location;
+cases = data.new_cases_smoothed_per_million;
+data_1 = table(date_reported, country, cases);
+
+%%
+%Segragating the data for individual countries
+data_India = (data_1(data_1.country == 'India',:));
+data_US = (data_1(data_1.country == 'United States',:));
+data_Canada = (data_1(data_1.country == 'Canada',:));
+data_Italy = (data_1(data_1.country == 'Italy',:));
+
+%%
+%Plotting the data in MATLAB
+figure(1);
+plot(data_India.date_reported, data_India.cases, 'LineWidth', 2);
+hold on
+plot(data_US.date_reported, data_US.cases, 'LineWidth', 2);
+plot(data_Canada.date_reported, data_Canada.cases, 'LineWidth', 2);
+plot(data_Italy.date_reported, data_Italy.cases, 'LineWidth', 2);
+hold off
+legend('India', 'United States', 'Canada', 'Italy');
+title('New Cases of COVID-19');
+ylabel('New cases per million')
+
+%%
+%Data adjustments
+data_US(1:9,:) = [];
+data_India(1,:) = [];
+data_Canada(1:5,:) = [];
+
+%%
+%Variations - Area plot
+figure(2);
+area(data_Italy.date_reported, [data_India.cases, data_Canada.cases,... 
+    data_US.cases, data_Italy.cases]);
+legend('India', 'United States', 'Canada', 'Italy');
+title('New Cases of COVID-19');
+ylabel('New cases per million');
+
+%%
+%Variations - Bar graph
+figure(3);
+bar(1:733, [data_India.cases, data_Canada.cases,... 
+    data_US.cases, data_Italy.cases], 0.5, 'stacked');
+legend('India', 'United States', 'Canada', 'Italy');
+title('New Cases of COVID-19');
+ylabel('New cases per million');
